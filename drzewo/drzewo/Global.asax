539d17a446d816ce86894f860ee65c49c3e50c22
Pawel <pawel83.pb@gmail.com>
2018-03-29 18:46:32 +0200

    init

---
+++ drzewo/drzewo/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="drzewo.MvcApplication" Language="C#" %>
---
+++ drzewo/drzewo/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(drzewo.Startup))]
+namespace drzewo
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
